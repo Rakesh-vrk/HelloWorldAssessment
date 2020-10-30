d8b00b0c9b83f8f1bb5d11e2b36fda2676a0a2aa
Rakesh Ventrapragada <v-ravent@microsoft.com>
2020-10-30 19:40:05 +0000

    initial commit

---
+++ src/test/java/com/assessment/workshop/HelloWorldAssessment/HelloWorldAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.workshop.HelloWorldAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
